8fd62b159eb73775edd422a2b41b93451a7b6f5f
diogo78oliveira <diogomigas78@gmail.com>
2024-10-07 12:44:17 +0100

    2

--- calculadora/app/src/main/java/com/example/calculadorapdm/MainActivity.kt
+++ calculadora/app/src/main/java/com/example/calculadorapdm/MainActivity.kt
@@ -1,47 +1,69 @@
-package com.example.calculadorapdm
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.* // Importing necessary libraries
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.calculadorapdm.ui.theme.CalculadoraPDMTheme
+import androidx.compose.ui.unit.dp
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CalculadoraPDMTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+@Composable
+fun Visor(text: String) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(text = text)
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Calculadora(onClick: (String) -> Unit) {
+
+    Column(modifier = Modifier.padding(30.dp)) {
+        Row() {
+            Button(onClick = { onClick("7") }, modifier = Modifier.weight(0.25f)) {
+                Text(text = "7")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = { onClick("8") }, modifier = Modifier.weight(0.25f)) {
+                Text(text = "8")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = { onClick("9") }, modifier = Modifier.weight(0.25f)) {
+                Text(text = "9")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = { onClick("/") }, modifier = Modifier.weight(0.25f)) {
+                Text(text = "/")
+            }
+        }
+        Row() {
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = { onClick("4") }, modifier = Modifier.weight(0.25f)) {
+                Text(text = "4")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = { onClick("5") }, modifier = Modifier.weight(0.25f)) {
+                Text(text = "5")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = { onClick("6") }, modifier = Modifier.weight(0.25f)) {
+                Text(text = "6")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = { onClick("*") }, modifier = Modifier.weight(0.25f)) {
+                Text(text = "*")
+            }
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CalculadoraPDMTheme {
-        Greeting("Android")
+fun WelcomePreview() {
+    var text by remember { mutableStateOf("0") }
+
+    Column() {
+        Visor(text = text)
+        Calculadora(onClick = { buttonText ->
+            text = buttonText
+        })
     }
 }
