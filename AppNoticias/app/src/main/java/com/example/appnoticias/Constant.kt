5b6f2e2a8e00a19e19384e0bee6c04613b6a1cae
diogo78oliveira <diogomigas78@gmail.com>
2024-11-26 11:59:57 +0000

    news

---
+++ AppNoticias/app/src/main/java/com/example/appnoticias/Constant.kt
@@ -0,0 +1,5 @@
+package com.example.appnoticias
+
+object Constant {
+    val apiKey = "dd41db845cce4e5d8af2e5780971d5ac"
+}
---
+++ AppNoticias/app/src/main/java/com/example/appnoticias/NewsViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.appnoticias
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.kwabenaberko.newsapilib.NewsApiClient
+import com.kwabenaberko.newsapilib.models.Article
+import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest
+import com.kwabenaberko.newsapilib.models.response.ArticleResponse
+
+class NewsViewModel: ViewModel() {
+
+    private val _articles = MutableLiveData<List<Article>>()
+    val articles: LiveData<List<Article>> = _articles
+
+    init {
+        fetchNewsTopHeadLines()
+    }
+
+    fun fetchNewsTopHeadLines(){
+        val newsApiClient = NewsApiClient(Constant.apiKey)
+        val request = TopHeadlinesRequest.Builder().language("en").build()
+
+        newsApiClient.getTopHeadlines(request, object : NewsApiClient.ArticlesResponseCallback {
+            override fun onSuccess(response: ArticleResponse?) {
+                response?.articles?.let {
+                    _articles.postValue(it)
+                }
+            }
+
+            override fun onFailure(throwable: Throwable?) {
+                if (throwable != null) {
+                    Log.i("NewsAPI Response Failed ", throwable.localizedMessage)
+                }
+            }
+
+
+        })
+    }
+}
