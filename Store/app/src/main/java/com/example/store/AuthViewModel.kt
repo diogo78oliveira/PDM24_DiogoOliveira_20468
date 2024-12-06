6e56c75be6d2023dcec0f3c74d72749077e87788
diogo78oliveira <diogomigas78@gmail.com>
2024-12-06 10:01:50 +0000

    Login

---
+++ Store/app/src/main/java/com/example/store/AuthViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.store
+
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel : ViewModel() {
+}
---
+++ Store/app/src/main/java/com/example/store/MyAppNavigation.kt
@@ -0,0 +1,25 @@
+package com.example.store
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.store.pages.LoginPage
+
+@Composable
+fun MyAppNavigation(modifier: Modifier = Modifier,authViewModel: AuthViewModel) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "login", builder = {
+        composable("login"){
+            LoginPage(modifier,navController,authViewModel)
+        }
+        composable("Registo"){
+            LoginPage(modifier,navController,authViewModel)
+        }
+        composable("login"){
+            LoginPage(modifier,navController,authViewModel)
+        }
+    })
+}
